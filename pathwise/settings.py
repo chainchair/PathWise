f7937d563c9f7c404d8ab2a5b1668af699a63ad8
samuelserpa <sdserpaz@eafit.edu.co>
2025-10-15 09:49:55 -0500

    password recovery

--- pathwise/settings.py
+++ pathwise/settings.py
@@ -99,6 +99,9 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# Enviar correos a la consola
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
@@ -124,3 +127,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
--- pathwise/urls.py
+++ pathwise/urls.py
@@ -16,6 +16,12 @@ urlpatterns = [
     path('about/', initialSurvey_views.about),
     path("survey/", include("initialSurvey.urls")), #includes all the urls from initialSurvey app
     path("accounts/", include("django.contrib.auth.urls")),  # login, logout, cambio de contraseña
+    
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='registration/password_reset_form.html'), name='password_reset'),
+    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(template_name='registration/password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='registration/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'), name='password_reset_complete'),
+
     path("users/", include("users.urls")),  # registro
     #path("login/", auth_views.LoginView.as_view(template_name="registration/login.html"), name="login"),
     path("roadmap/", include("roadmap.urls")),
